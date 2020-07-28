bfdb1c2df8a1b44ff44c381ad79e702b91142a76
Roger-luo <rogerluo.rl18@gmail.com>
2020-07-28 04:54:39 -0400

    change build

--- deps/build.jl
+++ deps/build.jl
@@ -1,2 +1,7 @@
 using Comonicon, IonCLI
-Comonicon.install(IonCLI, "ion"; compile=:min, sysimg=true)
+
+if "sysimg" in ARGS
+    Comonicon.build(IonCLI, "ion"; sysimg=true, filter_stdlibs=true, cpu_target="x86-64")
+else
+    Comonicon.install(IonCLI, "ion"; compile=:min)
+end
