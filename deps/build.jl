a8c536e8a68509eb6a4a9d35ced585b2fcb2c97f
Johnny Chen <johnnychen94@hotmail.com>
2020-09-03 14:27:32 +0800

    fallback to build sysimage if it is not available upstream

--- deps/build.jl
+++ deps/build.jl
@@ -1,2 +1,8 @@
 using Comonicon, IonCLI
-Comonicon.install(IonCLI)
+try
+    Comonicon.install(IonCLI)
+catch err
+    @warn "failed to download prebuilt sysimage" err
+    @info "fallback to build sysimage in your local machine, this could take a few minutes"
+    IonCLI.comonicon_build()
+end
