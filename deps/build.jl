be13a9694fc4244838c690d1f670e41d13d24951
Roger-luo <rogerluo.rl18@gmail.com>
2020-12-07 22:56:50 -0500

    update build

--- deps/build.jl
+++ deps/build.jl
@@ -1,2 +1,14 @@
-using IonCLI; IonCLI.comonicon_install()
-using IonBase; IonBase.copy_assets(joinpath(pkgdir(IonCLI), "build", "ion"))
+using IonBase
+using IonCLI
+using Comonicon: Comonicon, PATH, Options, BuildTools
+
+configs = Options.read_configs(IonCLI)
+@info "building application"
+BuildTools.build_application(IonCLI, configs)
+IonBase.copy_assets(joinpath(pkgdir(IonCLI), "build", "ion"))
+# pack tarball
+tarball = BuildTools.tarball_name(IonCLI, configs.name; application = true)
+@info "creating application tarball $tarball"
+cd(PATH.project(IonCLI, configs.application.path)) do
+    run(`tar -czvf $tarball $(configs.name)`)
+end
